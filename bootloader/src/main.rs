117c5d3bf6a8dd6315c886fc5ab2124f2fadd0bf
Ukuma012 <ukuma012@gmail.com>
2025-01-14 23:41:12 +0900

    kernelファイルの呼び出し

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -1,39 +1,58 @@
 #![no_main]
 #![no_std]
 
-use log::info;
-use uefi::Identify;
-use uefi::Result;
-use uefi::prelude::*;
-use uefi::proto::device_path::text::AllowShortcuts;
-use uefi::proto::device_path::text::DevicePathToText;
-use uefi::proto::device_path::text::DisplayOnly;
-use uefi::proto::loaded_image::LoadedImage;
+#[macro_use]
+extern crate alloc;
+
+use core::panic;
+
+use uefi::println;
+use uefi::proto::media::file::{File, FileInfo};
+use uefi::{
+    CStr16,
+    prelude::*,
+    proto::media::{
+        file::{FileAttribute, FileMode, FileType, RegularFile},
+        fs::SimpleFileSystem,
+    },
+};
 
 #[entry]
 fn efi_main() -> Status {
     uefi::helpers::init().unwrap();
 
-    print_image_path().unwrap();
+    let _kernel_entry_point_addr = load_elf("kernel.elf");
     boot::stall(10_000_000);
     Status::SUCCESS
 }
 
-fn print_image_path() -> Result {
-    let loaded_image = boot::open_protocol_exclusive::<LoadedImage>(boot::image_handle())?;
-    let device_path_to_text_handle =
-        *boot::locate_handle_buffer(boot::SearchType::ByProtocol(&DevicePathToText::GUID))?
-            .first()
-            .expect("DevicePathToText is missing");
+fn read_file(path: &str) -> RegularFile {
+    let sfs_handle = boot::get_handle_for_protocol::<SimpleFileSystem>().unwrap();
+    let mut root = boot::open_protocol_exclusive::<SimpleFileSystem>(sfs_handle)
+        .unwrap()
+        .open_volume()
+        .unwrap();
+    let mut buf = [0; 256];
+    let path = CStr16::from_str_with_buf(path, &mut buf).unwrap();
+    let file = root
+        .open(path, FileMode::Read, FileAttribute::empty())
+        .unwrap()
+        .into_type()
+        .unwrap();
 
-    let device_path_to_text =
-        boot::open_protocol_exclusive::<DevicePathToText>(device_path_to_text_handle)?;
+    match file {
+        FileType::Regular(file) => file,
+        FileType::Dir(_) => panic!("Not a file: {}", path),
+    }
+}
 
-    let image_device_path = loaded_image.file_path().expect("File path is not set");
-    let image_device_path_text = device_path_to_text
-        .convert_device_path_to_text(image_device_path, DisplayOnly(true), AllowShortcuts(false))
-        .expect("convert_device_path_to_text failed");
+fn load_elf(path: &str) {
+    let mut file = read_file(path);
+    let file_info = file.get_boxed_info::<FileInfo>().unwrap();
+    let file_size = file_info.file_size() as usize;
+    let mut buf = vec![0; file_size];
 
-    info!("Image Path: {}", &*image_device_path_text);
-    Ok(())
+    file.read(&mut buf).unwrap();
+
+    println!("{:?}", buf);
 }

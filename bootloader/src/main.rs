8177c4dd9e6c0d23226dd0f6fff525f1ae965ca6
Ukuma012 <ukuma012@gmail.com>
2025-01-26 20:41:37 +0900

    bootloaderを直していく

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -7,16 +7,10 @@ extern crate alloc;
 use boot::stall;
 use boot::{AllocateType, MemoryType};
 use common::elf::{Elf64, SegmentType};
-use common::graphic_info;
-use common::graphic_info::GraphicInfo;
-use common::mem_desc;
 use core::slice::from_raw_parts_mut;
 use core::{panic, usize};
 use log::info;
-use uefi::mem::memory_map::MemoryAttribute;
 use uefi::println;
-use uefi::proto::console::gop::GraphicsOutput;
-use uefi::proto::console::gop::PixelFormat;
 use uefi::proto::media::file::{File, FileInfo};
 use uefi::{
     CStr16,
@@ -33,11 +27,12 @@ const UEFI_PAGE_SIZE: usize = 4096;
 fn efi_main() -> Status {
     uefi::helpers::init().unwrap();
 
+    println!("get memory map");
+
     // load kernel
-    let kernel_entry_point_addr = load_elf("kernel.elf");
-    println!("entry_point: {}", kernel_entry_point_addr);
+    // let kernel_entry_point_addr = load_elf("kernel.elf");
+    // println!("entry_point: {}", kernel_entry_point_addr);
     stall(10_000_000);
-    // let graphic_info = init_graphic((800, 600));
 
     Status::SUCCESS
 }
@@ -87,6 +82,7 @@ fn load_elf(path: &str) -> u64 {
     }
 
     let pages = (dest_end - dest_start + UEFI_PAGE_SIZE - 1) / UEFI_PAGE_SIZE;
+
     boot::allocate_pages(
         AllocateType::Address(dest_start as u64),
         MemoryType::LOADER_DATA,
@@ -110,60 +106,4 @@ fn load_elf(path: &str) -> u64 {
     info!("Loaded ELF at: 0x{:x}", dest_start);
 
     elf.header().entry_point
-}
-
-fn init_graphic(resolution: (usize, usize)) -> GraphicInfo {
-    let gop_handle = boot::get_handle_for_protocol::<GraphicsOutput>().unwrap();
-    let mut gop = boot::open_protocol_exclusive::<GraphicsOutput>(gop_handle).unwrap();
-
-    let mode = gop
-        .modes()
-        .find(|mode| mode.info().resolution() == resolution)
-        .unwrap();
-    info!("Switching graphic mode...");
-    gop.set_mode(&mode).unwrap();
-
-    let mode_info = gop.current_mode_info();
-    let (width, height) = gop.current_mode_info().resolution();
-
-    GraphicInfo {
-        resolution: (width, height),
-        format: convert_pixel_format(mode_info.pixel_format()),
-        stride: mode_info.stride(),
-        framebuf_addr: gop.frame_buffer().as_mut_ptr() as u64,
-        framebuf_size: gop.frame_buffer().size(),
-    }
-}
-
-fn convert_pixel_format(pixel_format: PixelFormat) -> graphic_info::PixelFormat {
-    match pixel_format {
-        PixelFormat::Rgb => graphic_info::PixelFormat::Rgb,
-        PixelFormat::Bgr => graphic_info::PixelFormat::Bgr,
-        _ => panic!("Unsupported pixel format"),
-    }
-}
-
-fn convert_mem_attr(mem_attr: MemoryAttribute) -> u64 {
-    mem_attr.bits()
-}
-
-fn convert_mem_type(mem_type: MemoryType) -> mem_desc::MemoryType {
-    match mem_type {
-        MemoryType::RESERVED => mem_desc::MemoryType::Reserved,
-        MemoryType::LOADER_CODE => mem_desc::MemoryType::LoaderCode,
-        MemoryType::LOADER_DATA => mem_desc::MemoryType::LoaderData,
-        MemoryType::BOOT_SERVICES_CODE => mem_desc::MemoryType::BootServicesCode,
-        MemoryType::BOOT_SERVICES_DATA => mem_desc::MemoryType::BootServicesData,
-        MemoryType::RUNTIME_SERVICES_CODE => mem_desc::MemoryType::RuntimeServicesCode,
-        MemoryType::RUNTIME_SERVICES_DATA => mem_desc::MemoryType::RuntimeServicesData,
-        MemoryType::CONVENTIONAL => mem_desc::MemoryType::Conventional,
-        MemoryType::UNUSABLE => mem_desc::MemoryType::Unusable,
-        MemoryType::ACPI_RECLAIM => mem_desc::MemoryType::AcpiReclaim,
-        MemoryType::ACPI_NON_VOLATILE => mem_desc::MemoryType::AcpiNonVolatile,
-        MemoryType::MMIO => mem_desc::MemoryType::Mmio,
-        MemoryType::MMIO_PORT_SPACE => mem_desc::MemoryType::MmioPortSpace,
-        MemoryType::PAL_CODE => mem_desc::MemoryType::PalCode,
-        MemoryType::PERSISTENT_MEMORY => mem_desc::MemoryType::PersistentMemory,
-        MemoryType(value) => mem_desc::MemoryType::Other(value),
-    }
 }
